d720313a1053cb9c4cdb3f1e7b823ccf0e414395
snehal <thoratsnehal17@gmail.com>
2021-10-09 14:22:53 +0530

    Added: Snake&LadderUC1

---
+++ SnakeAndLadderUC2.java
@@ -0,0 +1,9 @@
+public class SnakeAndLadderUC2 {
+
+    public static  void main(String args[])
+    {
+        int player1Position = 0;
+        int rollsDieNumber =(int)Math.floor(Math.random() * 10) % 7;
+        System.out.println(rollsDieNumber);
+    }
+}
