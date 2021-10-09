e7ed1262e7779973ed404372f85c639ad081a928
snehal <thoratsnehal17@gmail.com>
2021-10-09 14:27:38 +0530

    Delete UC2

--- SnakeAndLadderUC2.java
+++
@@ -1,9 +0,0 @@
-public class SnakeAndLadderUC2 {
-
-    public static  void main(String args[])
-    {
-        int player1Position = 0;
-        int rollsDieNumber =(int)Math.floor(Math.random() * 10) % 7;
-        System.out.println(rollsDieNumber);
-    }
-}
